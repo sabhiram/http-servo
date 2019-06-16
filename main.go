f104626d0fa890bd33d59d705c0ee64ad355f7b9
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2019-06-15 17:32:57 -0700

    Attempt to cleanup servo stuff

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/sabhiram/go-rpio"
 	"os"
 	"time"
+
+	rpio "github.com/sabhiram/go-rpio"
+)
+
+const (
+	cFreqMultiplier = 20 * 10
 )
 
 func fatalOnErr(err error) {
@@ -14,16 +19,42 @@ func fatalOnErr(err error) {
 		os.Exit(1)
 	}
 }
-func main() {
-	fmt.Printf("Yo!\n")
-	fatalOnErr(rpio.Open())
-	defer rpio.Close()
-	pin := rpio.Pin(19)
-	pin.Mode(rpio.Pwm)
-	pin.Freq(50 * 200)
-	for i := 0; i < 24; i += 1 {
-		pin.DutyCycle(uint32(i), 200)
-		time.Sleep(200 * time.Millisecond)
+
+type PWMServo struct {
+	pin rpio.Pin
+}
+
+func NewPWMServo(bcmpid uint8) (*PWMServo, error) {
+	p := rpio.Pin(bcmpid)
+	p.Mode(rpio.Pwm)
+	p.Freq(50 * cFreqMultiplier)
+	return &PWMServo{pin: p}, nil
+}
+
+// SetAngle sets the servo angle to between 0 and 180 degrees.
+func (s *PWMServo) SetAngle(angle float32) error {
+	if angle < 0.0 || angle > 180.0 {
+		return fmt.Errorf("invalid angle (%d) [0 <= angle <= 180]", angle)
 	}
 
+	// DutyCycle of 1.0ms / 20ms corresponds to 0 deg
+	// 				1.5ms / 20ms corresponds to 90 deg
+	//				2.0ms / 20ms corresponds to 180 deg
+	s.pin.DutyCycle(uint32(((1.0+(angle/180.0))/20.0)*cFreqMultiplier), cFreqMultiplier)
+	return nil
+}
+
+func main() {
+	fatalOnErr(rpio.Open())
+	defer rpio.Close()
+
+	servo, err := NewPWMServo(19)
+	fatalOnErr(err)
+
+	for i := 0; i < 180; i += 1 {
+		servo.SetAngle(float32(i))
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	fmt.Printf("Bye!")
 }
